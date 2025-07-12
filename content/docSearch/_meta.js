8d820783ea085223f2c074633fa2bbe8eeadb580
zmtomorrow <mail@mingtian.ai>
2025-07-12 12:29:06 +0100

    fix

---
+++ content/docSearch/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  
+  // index: { title: 'Overview', type: 'doc' },
+  metadata: { title: 'by Metadata', type: 'doc' },
+  semantic: { title: 'by Semantic', type: 'doc' },
+  description: { title: 'by Description', type: 'doc' },
+}
